aadc5cc4a59641ecce2bef26a8bfa90c3246516d
Laura Vergara <Laura.Vergara@endava.com>
2023-01-05 17:03:59 -0500

    first try API

--- cypress/e2e/mainAPI.cy.js
+++ cypress/e2e/mainAPI.cy.js
@@ -1,5 +1,54 @@
 describe('empty spec', () => {
-    it('passes', () => {
-      cy.visit('https://pokeapi.co/')
+    it('creation', () => {
+      cy.request({
+        method: 'POST', 
+        url: 'https://petstore.swagger.io/v2/pet',
+        body: {
+            "id": 0,
+            "category": {
+                "id": 0,
+                "name": "string"
+            },
+            "name": 'pixie',
+            "photoUrls": [
+                "string"
+            ],
+            "tags": [
+                {
+                "id": 0,
+                "name": "string"
+                }
+            ],
+            "status": "available"
+        }
+    }).then((response) => {
+      expect(response.status).equal(200)
     })
+
+    })
+
+    it('find', () => {
+      cy.request({
+        method: 'GET', 
+        url: 'https://petstore.swagger.io/v2/pet/0',
+        header: {api_key:123},
+        failOnStatusCode:false
+      }).then((response) => {
+        expect(response.body).to.be.a('object')
+        expect(response.duration).to.be.lessThan(10000000)
+        // expect(response.body.name).equal('pixie')
+
+      })
+    })
+
+    it('delete', () => {
+      cy.request({
+        method: 'DELETE', 
+        url: 'https://petstore.swagger.io/v2/pet/0',
+        header: {api_key:123},
+        failOnStatusCode:false
+      })
+    })
+
+
   })
